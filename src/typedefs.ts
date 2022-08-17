ada568c5f5c900167003008dc6c3988ddce647af
Lenny Burdette <lenny@apollographql.com>
2022-08-17 11:24:01 -0400

    update to use flyby's locations schema

--- src/typedefs.ts
+++ src/typedefs.ts
@@ -1,8 +1,7 @@
-import { gql } from 'apollo-server';
-import { readFileSync } from 'fs';
-import * as path from 'path';
+import { gql } from "apollo-server";
+import { readFileSync } from "fs";
+import { resolve } from "path";
+
 export const typeDefs = gql(
-  readFileSync(path.resolve(__dirname, '../src/schema.graphql'), {
-    encoding: 'utf-8',
-  })
+  readFileSync(resolve(__dirname, "../src/schema.graphql"), "utf-8")
 );
