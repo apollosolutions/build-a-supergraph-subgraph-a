4b77b37908146a2082788f304d922f6240dd824c
Lenny Burdette <lenny@apollographql.com>
2022-09-16 10:36:08 -0400

    miscellaneous (#24)
    * switch deployment name to graphql
    * deploy to subgraph-a namespace
    * conditionally include otel env var based on environment
    * allow changing port with env var

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,6 @@ const server = new ApolloServer({
   plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })],
 });
 
-server.listen(4000).then(({ url }) => {
+server.listen(process.env.PORT ?? 4000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
